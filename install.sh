dbd238dc5497ac879664b255a298f76b65f2d160
Austin McDonald <austin@resim.ai>
2022-07-21 11:43:13 -0400

    add a gitconfig

--- install.sh
+++ install.sh
@@ -2,3 +2,4 @@ git clone https://github.com/ohmyzsh/ohmyzsh.git ~/.oh-my-zsh
 cp .zshrc ~/.zshrc
 cp .oh-my-zsh/custom/themes/austin.zsh-theme ~/.oh-my-zsh/custom/themes
 chsh -s $(which zsh)
+cp .gitconfig ~/.gitconfig
